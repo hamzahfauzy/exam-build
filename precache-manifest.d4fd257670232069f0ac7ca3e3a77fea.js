22dc4cf4a71c444a1c637c0b4e1127eea49b7722
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-05-28 21:37:19 +0700

    init

---
+++ precache-manifest.d4fd257670232069f0ac7ca3e3a77fea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87eb7e936fb518bf2237",
+    "url": "/css/chunk-vendors.7ba17a1a.css"
+  },
+  {
+    "revision": "e8d1d3031f47e7f1c27b7ab0bebb84cf",
+    "url": "/img/logo.e8d1d303.png"
+  },
+  {
+    "revision": "992d3bc3475a82a7d91abae25241d5da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a62cf4b2eda1c5215f90",
+    "url": "/js/app.a580e84b.js"
+  },
+  {
+    "revision": "87eb7e936fb518bf2237",
+    "url": "/js/chunk-vendors.4a618a53.js"
+  },
+  {
+    "revision": "ed53ef2d6f1a8197e925a6caf8069193",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
