ebc9823247eaaa832fe99cfcbd31f85f4c2b4e52
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-06-09 23:20:05 +0700

    fix sequence

---
+++ precache-manifest.36461c8090367d96621fe6c3511b782e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be06c4542c17c5f742ad",
+    "url": "/css/app.00a4d7b2.css"
+  },
+  {
+    "revision": "1d8a46080f73c4fa60ce",
+    "url": "/css/chunk-vendors.8ba15f19.css"
+  },
+  {
+    "revision": "fadc7e7eab9f87b94c7cfc10dd385f59",
+    "url": "/img/logo.fadc7e7e.png"
+  },
+  {
+    "revision": "3e2e2f30fca825714581a1aa0cb77428",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be06c4542c17c5f742ad",
+    "url": "/js/app.504f1825.js"
+  },
+  {
+    "revision": "1d8a46080f73c4fa60ce",
+    "url": "/js/chunk-vendors.c7161522.js"
+  },
+  {
+    "revision": "ed53ef2d6f1a8197e925a6caf8069193",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03ab6a010bd56b96e504ff914c988f0e.js"
+  "/precache-manifest.36461c8090367d96621fe6c3511b782e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tmc-exam"});
