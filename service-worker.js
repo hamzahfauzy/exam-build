9f42d0b44007f9d1c38f9646e2502a79ec79a1cd
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-07-07 23:04:17 +0700

    participant status and report link

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.219be8b11cb39a0d2146d002ace2ec38.js"
+  "/precache-manifest.9e814ad513799ff0d375ba2d223cbe21.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tmc-exam"});
